8d2503d3a65805c03267df3d1e6dcf1af4a49fa1
陈博先 <chenboxian@MacBook-Pro.local>
2025-05-18 14:38:50 +0800

    适配首页

--- code/server/app/controller/shop/home.js
+++ code/server/app/controller/shop/home.js
@@ -12,6 +12,26 @@ module.exports = app => {
                 order:[['sortNo', 'ASC']],
             });
             data.shopRecommendGoods=shopRecommendGoods;
+
+            let bannerGoods = await ctx.model.ShopGoods.findAll({
+                where:{bannerFlag:'1',goodsStatus:'U'},
+                order:[['sortNo', 'ASC']],
+            });
+            data.bannerGoods=bannerGoods;
+
+            let newGoods = await ctx.model.ShopGoods.findAll({
+                where:{newFlag:'1',goodsStatus:'U'},
+                order:[['sortNo', 'ASC']],
+            });
+            data.newGoods=newGoods;
+
+            let hotGoods = await ctx.model.ShopGoods.findAll({
+                where:{hotFlag:'1',goodsStatus:'U'},
+                order:[['sortNo', 'ASC']],
+            });
+            data.hotGoods=hotGoods;
+
+
             let subjects = await ctx.model.Subject.findAll({
                 where:{status:'0'},
                 order:[['createTime', 'desc']],
