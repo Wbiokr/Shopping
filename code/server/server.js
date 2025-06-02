347c96a2cdde0eb533b0d8429ace600b552f3c5e
陈博先 <chenboxian@MacBook-Pro.local>
2025-06-02 09:41:41 +0800

    egg

--- code/server/server.js
+++ code/server/server.js
@@ -1,16 +1,22 @@
 // server.js
-let egg
+console.log('Starting Egg.js server in Vercel environment...');
+console.log('Environment variables:', process.env);
+// server.js
+let egg = require('egg');
 try {
-  egg = require('egg');
   console.log('Egg.js 模块加载成功HAHA:', egg); // 添加日志
 } catch (error) {
   console.error('Egg.js 模块加载失败:', error);
   throw error;
 }
 
+console.log('Starting Egg.js server...');
+
 const app = egg.startCluster({
   baseDir: __dirname,
   port: process.env.PORT || 7001,
 });
+
+console.log('Egg.js app started successfully');
 
 module.exports = app;
